9c5e811dc7fce7bec0e2ea4b9981db72c2e168d7
Bilal <abusaadb@post.bgu.ac.il>
2016-11-08 20:56:31 +0200

    working on a minimal graph class - added a way to generate a random graph

--- graph.h
+++ graph.h
@@ -1,13 +1,15 @@
-#include<vector>
-
-
+// TODO add IFDEF thing
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <numeric>
 namespace graphs {
-
 // Represents an edge ending in vertex end, with weight w.
 struct Edge {
   int end;
   double w;
-}
+};
 
 // A class representing a graph, i.e a pair <V,E> of vertices and edges.
 // This implementation is an adjacency lists. Vertices are associated with a
@@ -19,7 +21,55 @@ class Graph {
     std::vector<std::vector<Edge>> adj_list;
 
   public:
-    size_t NumVerticies() const { return adj_list.size();}
+    int size() const { return adj_list.size();}
+    void AddV(int v, std::vector<Edge> neighbors) {
+      if (adj_list.size() <= v) {
+        adj_list.resize(v + 1);
+      }
+      adj_list[v] = std::move(neighbors);
+    }
+    const std::vector<Edge>& Neighbors(int v) const {return adj_list[v];}
+    Graph(int size): adj_list(size) {}
+    Graph() {}
+
+    long edges() {
+      return std::accumulate(std::begin(adj_list), std::end(adj_list), 0.0,
+          [] (long acc, auto&& next) { return acc += next.size();});
+    }
 };
 
+Graph randomGraph(int num_v) {
+  Graph result(num_v);
+  std::srand(std::time(0));
+  for (int i = 0; i < num_v; ++i) {
+    std::vector<Edge> edges;
+    for (int j = 0; j < num_v; ++j) {
+       if (i == j) {
+          continue;
+       }
+       // Flip a coin to decide wether to add edge <i, j>
+       if ((std::rand() % 2) == 1) {
+         edges.push_back({j, 1});
+       }
+    }
+    result.AddV(i, edges);
+  }
+
+  return result;
+}
+
+std::ostream& operator<<(std::ostream& os, const Edge& g) {
+  os << "(" << g.end << ", " << g.w << ")";
+  return os;
+}
+std::ostream& operator<<(std::ostream& os, const Graph& g) {
+  for (int i = 0; i < g.size(); ++i) {
+    os << "[" << i << "]->{";
+    for (const auto& e : g.Neighbors(i)) {
+      os << e;
+    }
+    os << "}\n";
+  }
+  return os;
+}
 } // namespace graphs.
--- graph_test.cc
+++ graph_test.cc
@@ -1,1 +1,10 @@
 // TODO this file
+#include "graph.h"
+using namespace std;
+using namespace graphs;
+
+int main() {
+  auto g = randomGraph(4000);
+  cout << "g has yay many edges " << g.edges();
+  return 0;
+ }
